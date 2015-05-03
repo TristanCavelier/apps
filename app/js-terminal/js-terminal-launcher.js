04da06544e13fd5b9a842fdf97fdcdc97753870b
TristanCavelier <t.cavelier@free.fr>
2015-05-03 10:34:17 +0000

    Create js-terminal-launcher.js

---
+++ app/js-terminal/js-terminal-launcher.js
@@ -0,0 +1,30 @@
+/*jslint indent: 2 */
+(function run(now) {
+  "use strict";
+  /*global window, document, toolbox, jsTerminal */
+  if (!now) {
+    if (document.body) {
+      return run(true) && null;
+    }
+    return window.addEventListener("load", run) && null;
+  }
+
+  window.help = document.createElement("div");
+  window.help.innerHTML = jsTerminal.help;
+
+  Object.keys(jsTerminal.commands).forEach(function (commandName) {
+    window[commandName] = jsTerminal.commands[commandName];
+  });
+
+  jsTerminal.create({
+    "root": "body",
+    "rc": "localstorage:.js-terminal-rc", // optional
+    "onError": function (reason) { window.ERR = reason; }, // optional
+    "onValue": function (value) { window.RET = value; }, // optional
+    "onAnswer": function (answer) { window.ANS = answer; } // optional
+  });
+
+  window.ecc = new toolbox.ExtendedCancellableChain();
+  window.esc = new toolbox.ExtendedStreamableChain();
+
+}());
